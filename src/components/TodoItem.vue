afbb1f2b43da6e2c8de8109d734c942883e199fc
Cuong Vu Hung (D9) <cuong.vuhung@vti.com.vn>
2025-11-06 17:35:05 +0700

    Show todo in FE

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -7,9 +7,8 @@ const { todo } = defineProps<{
 </script>
 
 <template>
-    <div >
-      <input type="checkbox" class=" " :checked="todo.is_done"/>
-
-    {{ todo.text }}
-    </div>
+  <div class="flex items-center space-x-2 border-y-indigo-500 p-3">
+    <input :checked="todo.is_done" type="checkbox" id="example1" class="h-4 w-4 rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400" />
+    <label for="example1" class="text-sm font-medium text-gray-700">{{ todo.text }}</label>
+  </div>
 </template>
